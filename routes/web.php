6aa5920d970fe8dad942dca04689f6c9e8266dd4
Yahya <yehyaali151989@gmail.com>
2020-11-01 16:17:30 +0200

    v1 to v5

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/{page}', 'AdminController@index');
